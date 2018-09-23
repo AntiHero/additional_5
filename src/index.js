ec4036d25f72323c3766031f5ea21c1ae2142db1
AntiHero <prophreaker@gmail.com>
2018-09-23 17:00:18 +0300

    additional_5/feature: implemente function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let configOpen = [];
+    let configClose = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        configOpen[i] = bracketsConfig[i][0];
+        configClose[i] = bracketsConfig[i][1];
+    }
+
+    let arr = str.split('');
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let index in configOpen) {
+            if (arr[i] == configOpen[index]) {
+                if (arr[i + 1] == configClose[index]) {
+                    arr.splice(i, 2);
+                    i = -1;
+                    break;
+                }
+            }
+        }
+    }
+    if (arr.length == 0) {
+        return true;
+    } else {
+        return false
+    }
 }
